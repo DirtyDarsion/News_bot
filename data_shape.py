bd2929f084a25d23d5cdda752b65441943e6ef33
DirtyDarsion <doktorbobikyaru@gmail.com>
2023-05-04 23:35:13 +0600

    Add timezone

---
+++ data_shape.py
@@ -0,0 +1,72 @@
+import requests
+from datetime import datetime
+
+conditions = {
+    'clear': 'ясно',
+    'partly-cloudy': 'малооблачно',
+    'cloudy': 'облачно',
+    'overcast': 'пасмурно',
+    'drizzle': 'морось',
+    'light-rain': 'небольшой',
+    'rain': 'дождь',
+    'moderate-rain': 'умеренно',
+    'heavy-rain': 'сильный',
+    'continuous-heavy-rain': 'длительный',
+    'showers': 'ливень',
+    'wet-snow': 'дождь',
+    'light-snow': 'небольшой',
+    'snow': 'снег',
+    'snow-showers': 'снегопад',
+    'hail': 'град',
+    'thunderstorm': 'гроза',
+    'thunderstorm-with-rain': 'дождь',
+    'thunderstorm-with-hail': 'гроза',
+}
+
+
+def get_data(yandex_api_key):
+    # Получение данных о валюте
+    try:
+        response = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
+        valute = response.json()['Valute']
+
+        usd = valute['USD']['Value']
+        usd = round(usd, 2)
+        eur = valute['EUR']['Value']
+        eur = round(eur, 2)
+    except requests.exceptions.JSONDecodeError:
+        usd = 'Ошибка'
+        eur = 'Ошибка'
+
+    # Получение данных о погоде
+    try:
+        response = requests.get('https://api.weather.yandex.ru/v2/forecast'
+                                '?lat=55.000693&lon=73.240121&[lang=ru-RU]',
+                                headers={'X-Yandex-API-Key': yandex_api_key})
+        weather = response.json()
+
+        city = weather['geo_object']['locality']['name']
+        temp_fact = weather['fact']['temp']
+        icon = f"https://yastatic.net/weather/i/icons/funky/dark/{weather['fact']['temp']}.svg"
+        condition_fact = weather['fact']['condition']
+        condition_fact = conditions[condition_fact]
+
+    except requests.exceptions.JSONDecodeError:
+        city = 'Ошибка'
+        temp_fact = 'Ошибка'
+        icon = 'Ошибка'
+        condition_fact = 'Ошибка'
+
+    # Формирование данных для вывода
+    data = {
+        'Время': datetime.now().strftime('%H:%M'),
+        'Дата': datetime.now().strftime('%d.%m.%y'),
+        'Курс доллара': usd,
+        'Курс евро': eur,
+        'Город': city,
+        'Температура сейчас': temp_fact,
+        'Иконка': icon,
+        'Погодное описание': condition_fact,
+    }
+
+    return data
--- main.py
+++ main.py
@@ -1,59 +1,36 @@
 import os
 import requests
+import pytz
 from datetime import datetime
 from dotenv import load_dotenv
 
-from aiogram import Bot, Dispatcher, executor, types
 import asyncio
 import aioschedule
+from aiogram import Bot, Dispatcher, executor, types
 
 from web_server import keep_alive
+from data_shape import get_data
 
 load_dotenv()
 
 USERNAME = os.getenv('USERNAME')
+YANDEX_API_KEY = os.getenv('YANDEX_API_KEY')
 TOKEN = os.getenv('TOKEN')
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-
-def get_data():
-    # Получение данных о валюте
-    try:
-        valute = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')
-        valute = valute.json()['Valute']
-
-        usd = valute['USD']['Value']
-        usd = round(usd, 2)
-        eur = valute['EUR']['Value']
-        eur = round(usd, 2)
-    except requests.exceptions.JSONDecodeError:
-        usd = 'Ошибка'
-        eur = 'Ошибка'
-
-    data = {
-        'Время': datetime.now().strftime('%H:%M'),
-        'Дата': datetime.now().strftime('%d.%m.%y'),
-        'Курс доллара': usd,
-        'Курс евро': eur
-    }
-
-    return data
-
-
-def convert_data():
-    data = get_data()
-    output = ''
-
-    for key, item in data.items():
-        output += f'{key}: {item}\n'
-    output = output[:-1]
-
-    return output
+tz = pytz.timezone('Asia/Omsk')
 
 
 async def send_news():
-    await bot.send_message(USERNAME, convert_data())
+    data = get_data(YANDEX_API_KEY)
+
+    text = ''
+    for key, item in data.items():
+        text += f'{key}: {item}\n'
+    text = text[:-1]
+
+    await bot.send_message(USERNAME, text)
 
 
 @dp.message_handler()
@@ -62,9 +39,10 @@ async def send_answer(message):
 
 
 async def scheduler():
-    aioschedule.every().day.at('8:30').do(send_news)
-    aioschedule.every().day.at('13:00').do(send_news)
-    aioschedule.every().day.at('19:00').do(send_news)
+    aioschedule.every().day.at('8:30', tz).do(send_news)
+    aioschedule.every().day.at('13:00', tz).do(send_news)
+    aioschedule.every().day.at('19:00', tz).do(send_news)
+    aioschedule.every().day.at('23:40', tz).do(send_news)
     while True:
         await aioschedule.run_pending()
         await asyncio.sleep(1)
