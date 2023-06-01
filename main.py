96c44c36d62794080d8cec8867ae8face76f2280
DirtyDarsion <doktorbobikyaru@gmail.com>
2023-06-01 23:58:45 +0600

    Fix errors

--- main.py
+++ main.py
@@ -260,9 +260,9 @@ async def on_startup(_):
 
     await bot.set_my_commands(commands)
 
-    await aioschedule.every().day.at('2:30').do(send_news, ADMIN)
-    await aioschedule.every().day.at('7:00').do(send_news, ADMIN)
-    await aioschedule.every().day.at('11:00').do(send_news, ADMIN)
+    aioschedule.every().day.at('2:30').do(send_news, ADMIN)
+    aioschedule.every().day.at('7:00').do(send_news, ADMIN)
+    aioschedule.every().day.at('11:00').do(send_news, ADMIN)
     await asyncio.create_task(scheduler())
 
 
